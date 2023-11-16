3041b13d29eb5e93b8c518f7834d7c237a37b43e
mahmoud_e <54193376+mektefaie@users.noreply.github.com>
2023-11-15 23:50:18 -0800

    finishing cleanup function

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,35 @@
-const UserChallenge = () => {
-  return <h2>user challenge</h2>;
-};
+import { useState } from 'react'
 
-export default UserChallenge;
+const UserChallenge = () => {
+  const [user, setUser] = useState(null)
+
+  // const login = () => {
+  //   setUser({ name: 'mamoushy' })
+  // }
+
+  const logout = () => {
+    setUser(null)
+  }
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>hello there, {user.name}</h4>
+          <button className='btn' onClick={logout}>
+            logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please Login</h4>
+          <button className='btn' onClick={() => setUser({ name: 'mamoushy' })}>
+            login
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default UserChallenge
