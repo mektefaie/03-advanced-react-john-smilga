97f406fd66d3b909e4ab7b122bbe5ccfa84b4eef
mahmoud_e <54193376+mektefaie@users.noreply.github.com>
2023-11-20 23:57:31 -0800

    finishing 119. Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import Starter from './tutorial/03-conditional-rendering/starter/07-user-challenge.jsx'
+import Example from './tutorial/04-project-structure/starter/Example'
+import Navbar from './tutorial/04-project-structure/starter/Navbar'
+import { Home, About } from './tutorial/04-project-structure/starter/Pages'
+
 function App() {
   return (
     <div className='container'>
-      <Starter />
+      <Example />
     </div>
   )
 }
