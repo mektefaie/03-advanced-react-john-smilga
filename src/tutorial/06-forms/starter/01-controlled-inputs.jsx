41afc9a05df6f67577c199cee21b1a9374847880
mahmoud_e <54193376+mektefaie@users.noreply.github.com>
2023-11-24 00:02:59 -0800

    finishing 124. controlled inputs

--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,48 @@
+import { useState } from 'react'
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
-};
-export default ControlledInputs;
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(name, email)
+  }
+
+  return (
+    <div>
+      <form className='form' onSubmit={handleSubmit}>
+        <h4>Add User</h4>
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            name
+          </label>
+          <input
+            type='text'
+            className='form-input'
+            id='name'
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <div className='form-row'>
+          <label htmlFor='email' className='form-label'>
+            email
+          </label>
+          <input
+            type='text'
+            className='form-input'
+            id='email'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+      {/* render users below */}
+    </div>
+  )
+}
+export default ControlledInputs
