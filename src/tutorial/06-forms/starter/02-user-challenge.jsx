e24f6cd1b913436a41c2d184db482ea3c3474ac3
mahmoud_e <54193376+mektefaie@users.noreply.github.com>
2023-12-01 15:36:23 -0800

    finishing 128. user challenge - remove user

--- src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,13 +1,41 @@
+import { useState } from 'react'
+import { data } from '../../../data'
+
 const UserChallenge = () => {
+  const [name, setName] = useState('')
+  const [users, setUsers] = useState(data)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (!name) return
+    const fakeId = Date.now()
+    const newUser = { id: fakeId, name }
+    const updateUsers = [...users, newUser]
+    setUsers(updateUsers)
+    setName('')
+  }
+
+  const removeUser = id => {
+    const updateUsers = users.filter(user => user.id !== id)
+    setUsers(updateUsers)
+    setName('')
+  }
+
   return (
     <div>
-      <form className='form'>
+      <form className='form' onSubmit={handleSubmit}>
         <h4>Add User</h4>
         <div className='form-row'>
           <label htmlFor='name' className='form-label'>
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type='text'
+            className='form-input'
+            id='name'
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
         </div>
 
         <button type='submit' className='btn btn-block'>
@@ -15,7 +43,23 @@ const UserChallenge = () => {
         </button>
       </form>
       {/* render users below */}
+      <h2>users</h2>
+      {users.map(user => {
+        return (
+          <div key={user.id}>
+            <h4>{user.name}</h4>
+            <button
+              onClick={() => {
+                removeUser(user.id)
+              }}
+              className='btn'
+            >
+              remove
+            </button>
+          </div>
+        )
+      })}
     </div>
-  );
-};
-export default UserChallenge;
+  )
+}
+export default UserChallenge
